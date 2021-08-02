9167f07a051d7e789af9e0915e0f98c169dabf6e
ANDRÉ MYSZKO <andre.myszko@gmail.com>
2021-08-02 16:58:13 -0300

    update. questao4

--- Questao4.java
+++ Questao4.java
@@ -12,10 +12,27 @@ public class Questao4 {
         System.out.println("Insira o FINAL do intervalo:");
         int fim_intervalo = scanner.nextInt();
 
-        while(inicio_intervalo != fim_intervalo){
-            int result = multiplicador * inicio_intervalo;
-            System.out.println(multiplicador + " x " + inicio_intervalo + " = " + result);
-            inicio_intervalo++;
+        scanner.close();
+
+        int dif_intervalo = fim_intervalo - inicio_intervalo;
+
+        if(inicio_intervalo > fim_intervalo){
+            System.out.println("Erro: O início é maior que o fim do intervalo.");
+        }
+        else if (dif_intervalo > 10){
+            System.out.println("Erro: O intervalo entre início e fim não pode ser maior do que 10.");
+        }
+        else if(inicio_intervalo > 1000 || inicio_intervalo < 0 || fim_intervalo > 1000 || fim_intervalo < 0){
+            System.out.println("Erro: O valores de intervalo devem ser maiores que 0 e menores que 1000.");
+        }
+        else if(multiplicador > 1000){
+            System.out.println("Erro: Mutiplicador não pode ser maior que 1000.");
+        }else{    
+            while(inicio_intervalo != fim_intervalo){
+                int result = multiplicador * inicio_intervalo;
+                System.out.println(multiplicador + " x " + inicio_intervalo + " = " + result);
+                inicio_intervalo++;
+            }
         }
 
     }
